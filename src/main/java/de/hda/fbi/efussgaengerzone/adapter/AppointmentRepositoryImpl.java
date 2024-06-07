eb5a434c02b0cf158d2e170ef811b9b1c43cf134
Tuandung Le <tuandung.le@stud.h-da.de>
2024-06-07 07:40:11 +0200

    chore: clean redundancies

--- src/main/java/de/hda/fbi/efussgaengerzone/adapter/AppointmentRepositoryImpl.java
+++ src/main/java/de/hda/fbi/efussgaengerzone/adapter/AppointmentRepositoryImpl.java
@@ -4,10 +4,9 @@ import de.hda.fbi.efussgaengerzone.domain.model.appointment.Appointment;
 import de.hda.fbi.efussgaengerzone.domain.model.appointment.AppointmentRepository;
 import de.hda.fbi.efussgaengerzone.domain.usecase.AppointmentNotFoundException;
 import org.springframework.stereotype.Repository;
-<<<<<<< src/main/java/de/hda/fbi/efussgaengerzone/adapter/AppointmentRepositoryImpl.java
+
 
 import java.util.*;
-=======
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -15,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
->>>>>>> src/main/java/de/hda/fbi/efussgaengerzone/adapter/AppointmentRepositoryImpl.java
+
 
 @Repository
 public class AppointmentRepositoryImpl implements AppointmentRepository {
@@ -35,28 +34,6 @@ public class AppointmentRepositoryImpl implements AppointmentRepository {
 
     @Override
     public Appointment delete(UUID shopId, UUID appointmentId) {
-<<<<<<< src/main/java/de/hda/fbi/efussgaengerzone/adapter/AppointmentRepositoryImpl.java
-        List<Appointment> shopAppointments = appointmentsByShop.getOrDefault(shopId, new ArrayList<>());
-
-        Appointment deletedAppointment = null;
-        Iterator<Appointment> it = shopAppointments.iterator();
-        while (it.hasNext()) {
-            Appointment appointment = it.next();
-            if (appointmentId.equals(appointment.id())) {
-                it.remove();
-                deletedAppointment = appointment;
-            }
-        }
-
-        if (deletedAppointment != null) {
-            if (shopAppointments.isEmpty()) {
-                appointmentsByShop.remove(shopId);
-            }
-            return deletedAppointment;
-        } else {
-            throw new AppointmentNotFoundException(String.format("Appointment %s not found for shop %s", appointmentId, shopId));
-        }
-=======
         List<Appointment> shopAppointments = appointmentsByShop.getOrDefault(shopId,
                 new ArrayList<>());
         Appointment appointment = shopAppointments.stream()
@@ -65,11 +42,12 @@ public class AppointmentRepositoryImpl implements AppointmentRepository {
                 .orElseThrow(() -> new
                         AppointmentNotFoundException(String.format("Appointment %s not found for shop %s",
                         appointmentId, shopId)));
+
         shopAppointments.remove(appointment);
         if (shopAppointments.isEmpty()) {
                 appointmentsByShop.remove(shopId);
             }
         return appointment;
->>>>>>> src/main/java/de/hda/fbi/efussgaengerzone/adapter/AppointmentRepositoryImpl.java
+
     }
 }
