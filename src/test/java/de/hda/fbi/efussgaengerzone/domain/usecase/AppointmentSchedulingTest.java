11a0577b134b29762cc0760d738512b1da5caa61
Marcel <marcel.anton1998@gmail.com>
2024-06-27 23:59:03 +0200

    Added AppointmentScheduling.findNextAppointment tests Closes #22

--- src/test/java/de/hda/fbi/efussgaengerzone/domain/usecase/AppointmentSchedulingTest.java
+++ src/test/java/de/hda/fbi/efussgaengerzone/domain/usecase/AppointmentSchedulingTest.java
@@ -8,16 +8,19 @@ import de.hda.fbi.efussgaengerzone.domain.model.appointment.AppointmentRepositor
 import de.hda.fbi.efussgaengerzone.domain.model.shop.OpeningHours;
 import de.hda.fbi.efussgaengerzone.domain.model.shop.WeeklyOpeningHours;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -60,7 +63,7 @@ public class AppointmentSchedulingTest {
 
         shop = new Shop(shopId, "Test Shop", "Description", new HashSet<>(), weeklyOpeningHours, new HashSet<>(), true, 30, "owner@example.com");
 
-        appointment = new Appointment(UUID.randomUUID(), "John Doe", null, "123456789", LocalDateTime.now(), null);
+        appointment = new Appointment(UUID.randomUUID(), "John Doe", null, "123456789", LocalDateTime.now().plusHours(1), Duration.ofMinutes(15));
 
         // Simple filter that accepts all appointments
         filter = appointment -> true;
@@ -84,5 +87,26 @@ public class AppointmentSchedulingTest {
         Collection<Appointment> results = appointmentScheduling.searchAppointments(shopId, Set.of(filter));
 
         assertEquals(0, results.size());
+    }
+
+    @Test
+    public void whenNextAppointmentExists() {
+        when(shopRepository.findById(shopId)).thenReturn(Optional.of(shop));
+        when(appointmentRepository.findForShopId(shopId)).thenReturn(List.of(appointment));
+
+        Optional<Appointment> nextAppointment = appointmentScheduling.findNextAppointment(shopId);
+
+        Assertions.assertThat(nextAppointment).isPresent();
+        assertEquals(appointment, nextAppointment.get());
+    }
+
+    @Test
+    public void whenNextAppointmentDoesntExist() {
+        when(shopRepository.findById(shopId)).thenReturn(Optional.of(shop));
+        when(appointmentRepository.findForShopId(shopId)).thenReturn(List.of());
+
+        Optional<Appointment> nextAppointment = appointmentScheduling.findNextAppointment(shopId);
+
+        Assertions.assertThat(nextAppointment).isEmpty();
     }
 }
